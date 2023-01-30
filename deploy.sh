ffcc1e81dd7313853dc417ab6dd305e6f719c945
gileneo <gileneusz@gmail.com>
2023-01-31 07:21:50 +0900

    totally new

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:gileneusz/medicalshift.git master:gh-pages
+git push -f git@github.com:gileneusz/medicalshift2.git master:gh-pages
 #git push -f git@github.com:gileneusz/gileneusz.github.io.git master:gh-pages
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-     ? '/medicalshift/'
+     ? '/medicalshift2/'
     //? '/gileneusz.github.io/'
     : '/'
 }
